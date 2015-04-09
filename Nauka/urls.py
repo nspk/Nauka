8f3b9c5728a2ddc76738f63883bc2d6c2a6d25a1
nspk <LuKasZ12#>
2015-04-09 22:25:31 +0200

    Poprawiony widok adminsitracji temperatura

--- Nauka/urls.py
+++ Nauka/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from temp.views import TermometrListView
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'Nauka.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^termometr/', TermometrListView.as_view())
 ]
--- temp/views.py
+++ temp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Termometr
+
+class TermometrListView(ListView):
+    model = Termometr
