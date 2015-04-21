6d74f705d76ed540c80ffac3e397e31c31d8a7d9
nspk <LuKasZ12#>
2015-04-21 22:04:59 +0200

    Dodanie formularza standardowego i z modelu

--- contact/forms.py
+++ contact/forms.py
@@ -1,7 +1,19 @@
 from django import forms
 from .models import Message
 
-class MessageForm(forms.ModelForm):
+class MessageForm(forms.ModelForm): #dziedziczy z ModelForm co znaczy ze pracuje od razu na modelu
     class Meta:
         model = Message
         fields = ('uzytkownik', 'email', 'tresc')
+
+
+class ContactForm(forms.Form): #bezwykorzystania Modelform
+    name = forms.CharField()
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea())
+
+    def clean_name(self):
+        data = self.cleaned_data['name']
+        if "D" not in data:
+            raise forms.ValidationError("Musisz mieć imię zawierajace 'D'")
+        return data
--- contact/views.py
+++ contact/views.py
@@ -1,13 +1,13 @@
 from django.views.generic import DetailView, FormView
 from .models import Message
-from .forms import MessageForm
+from .forms import MessageForm, ContactForm
 
-#class MessageDetailView(DetailView):
-#   model = Message
-
-class MessageAddView(FormView):
-    form_class = MessageForm
+class MessageAddView(FormView): #dziedziczy z ModelForm co znaczy ze pracuje od razu na modelu
+    form_class = ContactForm #MessageForm
     template_name = 'contact/message_form.html'
+    success_url = "/"
 
-
-# Create your views here.
+    #uruchamiany kiedy jest poprawny
+    def form_valid(self, form):
+        form.save() #bo model form jest instancja ModelForm i ma metode save dla MessageForm
+        return super(MessageAddView, self).form_valid(form)
